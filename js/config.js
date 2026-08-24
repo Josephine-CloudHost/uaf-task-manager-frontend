11bc4bfa04f548b3f2facb45693d32e524de48f0
Josephine-CloudHost <chrisckindness@gmail.com>
2026-08-23 23:00:02 -0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -4,7 +4,7 @@
  * This is the API url
  *************************************************************/
 const CONFIG = {
-  API_URL: ''https://script.google.com/macros/s/AKfycbwzaMVLvA-GRuw0EzgmudNnkVQ733otZmgk8Nv40b6WHvWqmuwSCyLLoF2E9a_x1exKkA/exec'',
+  API_URL: 'https://script.google.com/macros/s/AKfycbwzaMVLvA-GRuw0EzgmudNnkVQ733otZmgk8Nv40b6WHvWqmuwSCyLLoF2E9a_x1exKkA/exec',
   ORG_NAME: 'Upskill Africa Foundation (UAF)',
   APP_NAME: 'UAF Tasks Tracker',
 };
