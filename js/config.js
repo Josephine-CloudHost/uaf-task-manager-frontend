5035cb92a403a9db37239bf24b3200ca79f8343b
Josephine-CloudHost <chrisckindness@gmail.com>
2026-08-23 22:32:29 -0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,12 +1,10 @@
 /*************************************************************
  * CONFIG
  * -----------------------------------------------------------
- * Paste your Apps Script Web App URL below (Deploy > New
- * deployment > Web app > copy the URL ending in /exec).
- * That's the only edit required to make this front end work.
+ * This is the API url
  *************************************************************/
 const CONFIG = {
   API_URL: 'https://script.google.com/macros/s/AKfycbwzaMVLvA-GRuw0EzgmudNnkVQ733otZmgk8Nv40b6WHvWqmuwSCyLLoF2E9a_x1exKkA/exec',
   ORG_NAME: 'UAF-LR',
-  APP_NAME: 'Field Ledger',
+  APP_NAME: 'UAF Tasks Tracker',
 };
